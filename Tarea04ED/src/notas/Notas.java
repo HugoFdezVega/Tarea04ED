41a1a3b0dec6683e74ef262ac5616a538c392e5c
profi <profi@DESKTOP-695H3QE>
2022-02-19 18:05:37 +0100

    Utiliza las funciones de refactorización
    Para sustituir el código que se encarga de especificar la calificación
    en función de la nota por un método que llamarás "cali", que será
    público, de forma que se incluyan los comentarios para la documentación
    si ello es posible. Muestra cómo se hace en los dos entornos. (En
    Eclipse y en Netbeans). Sube las modificaciones al repositorio de
    Github, siendo el comentario el enunciado de este ejercicio.

--- Tarea04ED/src/notas/Notas.java
+++ Tarea04ED/src/notas/Notas.java
@@ -6,23 +6,33 @@ public class Notas {
 
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);// Para pedir números por teclado
-		int numeroIntroducido= 0;
+		int notaIntroducida= 0;
 		String calificacionFinal = "";
 		System.out.print("Introduzca una nota: ");
-		numeroIntroducido = reader.nextInt();
+		notaIntroducida = reader.nextInt();
 
-		if (numeroIntroducido >= 0 && numeroIntroducido < 5)
-			calificacionFinal = "Suspenso";
-		else if (numeroIntroducido >= 5 && numeroIntroducido < 7)
-			calificacionFinal = "Aprobado";
-		else if (numeroIntroducido >= 7 && numeroIntroducido < 9)
-			calificacionFinal = "Notable";
-		else if (numeroIntroducido >= 9 && numeroIntroducido <= 10)
-			calificacionFinal = "Sobresaliente";
-		else
-			calificacionFinal = "La nota introducida no es correcta";
+		calificacionFinal = cali(notaIntroducida);
 
 		System.out.println(calificacionFinal);
 		reader.close();
+	}
+
+	/**
+	 * @param notaIntroducida
+	 * @return
+	 */
+	public static String cali(int notaIntroducida) {
+		String calificacionFinal;
+		if (notaIntroducida >= 0 && notaIntroducida < 5)
+			calificacionFinal = "Suspenso";
+		else if (notaIntroducida >= 5 && notaIntroducida < 7)
+			calificacionFinal = "Aprobado";
+		else if (notaIntroducida >= 7 && notaIntroducida < 9)
+			calificacionFinal = "Notable";
+		else if (notaIntroducida >= 9 && notaIntroducida <= 10)
+			calificacionFinal = "Sobresaliente";
+		else
+			calificacionFinal = "La nota introducida no es correcta";
+		return calificacionFinal;
 	}
 }
